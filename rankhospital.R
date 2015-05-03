3aa8e7453a3cd56b3f6f6ba68a86f3189b5930e0
James Schacht <pascal888@yahoo.com>
2015-05-03 09:52:39 -0400

    reverted back to pre- commented - I must have introduced and error when commenting.  No time to fix it now.  Note to self - if I need good comments look on the previous commit.
    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- rankhospital.R
+++ rankhospital.R
@@ -1,6 +1,6 @@
+## Need to submit() for rankhospital part 4 of the assignment still
 rankhospital <- function(state, outcome, num = "best") {
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-## Begin state "if" statement - encapsulates all code - if the exact two-letter uppercase abbreviation for state is not found in the dataset return "invalid state".
     if (state %in% data$State) {
         data[,11] <- as.numeric(data[,11])
         data[,17] <- as.numeric(data[,17])
@@ -10,28 +10,24 @@ rankhospital <- function(state, outcome, num = "best") {
         for (i in 1:length(unique_States)) {
             statedata <- split(data, data$State)
         }
-        s <- 55  ## s will hold the state with the outcome value being sought.  If s does not get set to a number within - 1:length(unique_States) - it will return "NA"
-        final <- as.vector(c())
-        if (num == "best" || num == "worst") {y <- 1} else {y <- num}  ## y will hold the final position in the data frame for the ranked hospital.  best and worst is toggled to first postion in the data frame by using order and a "-" in front of the column name on the sort to make it decending order
-## Begin the large if statement that checks to ensure the outcome is valid
+        if (num == "best" || num == "worst") {y <- 1} else {y <- num}
         if (outcome == "heart attack" || outcome == "heart failure" || outcome == "pneumonia") {
- ## Begin specific outcome processing
             if (outcome == "heart attack") {
-## Set sort order to ascending for each state for best and any specific number for "num"
                 for (i in 1:length(unique_States)) {
                     statedata[[i]] <- statedata[[i]][order(statedata[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,statedata[[i]]$Hospital.Name),]
                 }
-## Set sort order to descending for worst for each state
                 if (num == "worst"){
                     for (i in 1:length(unique_States)) {
                         statedata[[i]] <- statedata[[i]][order(-statedata[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,statedata[[i]]$Hospital.Name),]
                     }
                 }
-                ## Set s to state data frame index for heart attack ratings
-                    for (i in 1:length(unique_States)) {
+                ## find state
+                final <- as.vector(c())
+                s <- 55
+                for (i in 1:length(unique_States)) {
                     if (num == "worst" || num == "best") {num <- 1}
                     if (num > nrow(statedata[[i]])) {
-                         final <- "NA"
+                        final <- "NA"
                     } else {
                         if (!any(is.na(statedata[[i]][y,2])) && statedata[[i]][y,2] == state) {
                             s <- i
@@ -39,8 +35,9 @@ rankhospital <- function(state, outcome, num = "best") {
                     }
                 }
                 if (s < 55) {final<- statedata[[s]][y,1]}
-            return(final)
-            # End Heart Attack
+                return(final)
+            }
+
             if (outcome == "heart failure") {
                 for (i in 1:length(unique_States)) {
                     statedata[[i]] <- statedata[[i]][order(statedata[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,statedata[[i]]$Hospital.Name),]
@@ -50,7 +47,9 @@ rankhospital <- function(state, outcome, num = "best") {
                         statedata[[i]] <- statedata[[i]][order(-statedata[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,statedata[[i]]$Hospital.Name),]
                     }
                 }
-                ## Set s to state data frame index for heart failure ratings
+                ## find state
+                final <- as.vector(c())
+                s <- 55
                 for (i in 1:length(unique_States)) {
                     if (num == "worst" || num == "best") {num <- 1}
                     if (num > nrow(statedata[[i]])) {
@@ -64,7 +63,7 @@ rankhospital <- function(state, outcome, num = "best") {
                 if (s < 55) {final<- statedata[[s]][y,1]}
                 return(final)
             }
-            ## End Heart Failure
+
             if (outcome == "pneumonia") {
                 for (i in 1:length(unique_States)) {
                     statedata[[i]] <- statedata[[i]][order(statedata[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia,statedata[[i]]$Hospital.Name),]
@@ -74,7 +73,9 @@ rankhospital <- function(state, outcome, num = "best") {
                         statedata[[i]] <- statedata[[i]][order(-statedata[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia,statedata[[i]]$Hospital.Name),]
                     }
                 }
-                ## Set s to state data frame index for pneumonia ratings
+                ## find state
+                final <- as.vector(c())
+                s <- 55
                 for (i in 1:length(unique_States)) {
                     if (num == "worst" || num == "best") {num <- 1}
                     if (num > nrow(statedata[[i]])) {
@@ -88,15 +89,13 @@ rankhospital <- function(state, outcome, num = "best") {
                 if (s < 55) {final<- statedata[[s]][y,1]}
                 return(final)
             }
-            ## End Pneumonia
-## End specific outcome processing
+
         } else {
-## End the large if statement that checks to ensure the outcome is valid
             stop("invalid outcome")
         }
     } else {
-## End state "if" statement - encapsulates all code - if the exact two-letter uppercase abbreviation for state is not found in the dataset return "invalid state"
         stop("invalid state")
+
     }
-}
+
 }
